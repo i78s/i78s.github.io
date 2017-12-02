2a59102c55a4dffe3424c933f5ae579c70d0c53d
稲葉 聡 <inaba_satoshi@liginc.co.jp>
2017-12-03 02:09:39 +0900

    docsディレクトリにhtmlを移動

--- ligblog-sample/restangular-sample/demo/js/services.js
+++
@@ -1,9 +0,0 @@
-angular.module('services',[]);
-angular.module('services').factory('MemberModel', [
-    'Restangular',
-    'ApiUrlConstant',
-    function (Restangular,ApiUrlConstant) {
-        var account = Restangular.all(ApiUrlConstant.member);
-        return account;
-    }
-]);
